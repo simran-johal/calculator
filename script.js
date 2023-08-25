045367b21e371fc72c76e8229352db97201f642b
Simran Johal <134616481+simran-johal@users.noreply.github.com>
2023-08-25 17:34:02 +0100

    Add basic calculator data inputting functionality
    - Complete basic 2 number calculations

--- script.js
+++ script.js
@@ -3,13 +3,111 @@
 
 
 
-// FUNCTIONS FOR ADD, MINUS, TIMES, DIVIDE INSIDE OBJECT
-// CALC OPERATION 3 VARIABLES TO MANAGE DYNAMIC VALUES
-// CALC OPERATION FUNCTION THAT TAKES OPERATION & NUMBERS
 
-let primaryNumber = []
-let secondaryNumber = []
 
+
+// FUNCTIONS THAT POPULATE THE DISPLAY
+// ADD LISTENERS TO ALL BUTTONS
+let primaryNumber = null
+let secondaryNumber = null
+let primaryOperator = null
+let buttonValue = [] 
+
+let currentNumber
+let currentOperator
+let buttons = document.querySelectorAll('.buttons') 
+
+let equalSelected = false
+let operatorSelected = false
+
+
+
+//------------------------------------------------------------------------------------
+
+
+buttons.forEach((button) => {
+    button.addEventListener('click', function (event) {
+
+
+
+//------------------------------------------------------------------------------------
+
+       // NUMBER SELECTED LOGIC
+       if (/^[0-9]$/.test(event.target.innerHTML)) { 
+
+            // WHEN NUMBER SELECTED AFTER OPERATOR THIS RESETS SO NEW CURRENTNUMBER CAN BUILD
+            if (operatorSelected) { 
+                buttonValue = []
+                operatorSelected = false
+            }
+
+            // CREATES A BUTTONVAL ARRAY THEN TURNS INTO NUMBER AND JOINS AND ASSIGNS TO CURRENTNUM
+            buttonValue.push(Number(event.target.innerHTML))
+            currentNumber = Number(buttonValue.join(""))
+       
+            console.log("current number", currentNumber)
+            
+        }
+
+       // OPERATOR SELECTED LOGIC
+       if (/[\+\-x÷]/.test(event.target.innerHTML)) {
+            
+            operatorSelected = true
+
+            switch (event.target.innerHTML) {
+                case '+':
+                    primaryOperator = "add";
+                    break;
+                case '-':
+                    primaryOperator = "subtract";
+                    break;
+                case '÷':
+                    primaryOperator = "divide";
+                    break;
+                case 'x':
+                    primaryOperator = "multiply";
+                    break;
+            }
+            
+            
+
+
+
+            if (primaryNumber == null) {
+                primaryNumber = currentNumber;
+            } else if (primaryNumber !== null && secondaryNumber == null) {
+                secondaryNumber = currentNumber }
+            
+            buttonValue = []
+            currentNumber = null;
+            console.log("current number", currentNumber)
+            console.log("primary number", primaryNumber)
+            console.log("Secondary number", secondaryNumber)
+            console.log("primary operator", primaryOperator)
+       }
+
+//------------------------------------------------------------------------------------
+
+
+       // EQUALS SELECTED LOGIC TO USE OUTPUTS OF OPERATOR/NUMBER LOGIC ABOVE
+       if (/=/.test(event.target.innerHTML)) {
+
+        equalSelected = true
+        secondaryNumber = currentNumber
+        runOperate(primaryOperator, primaryNumber, secondaryNumber)
+        
+       }
+
+    })
+
+})
+
+
+//------------------------------------------------------------------------------------
+
+
+
+let result
 let operatorObj = { 
     add: (num1,num2) => num1 + num2,
     subtract: (num1,num2) => num1 - num2,
@@ -17,63 +115,33 @@ let operatorObj = {
     multiply: (num1,num2) => num1 * num2,
 }
 
-function operate(oper, num1, num2) {
-    if (operatorObj[oper]) {
-        return operatorObj[oper](num1, num2)
-    }
-    else return null
-}
 
-
-
-// STORE FIRST+SECOND NUMBER INPUT + OPERATION
-// CALL OPERATE() WITH THEM WHEN '=' PRESSED
-
-function runOperate() {
-    if (buttonValue == '=') {
-        operate(operation, primaryNumber, secondaryNumber) // CALLING THE FUNC
-    }
+function runOperate(primOper, primNum, seconNUM) {
     
+    result = operatorObj[primOper](primNum, seconNUM)
+    console.log("runOperate result", result)
+    return result
 
 }
+// RUNOPERATE FUNCTION WORKS
+ console.log("personal checks: runOperate check", runOperate("divide", 10, 20)) 
+ 
 
 
 
-// WORKOUT HOW TO STORE NUMBERS IN AN ARRAY TILL WE CLICK AN OPERATOR
-// AT WHICH POINT THOSE NUMBERS MADE INTO SINGLE STRING THEN CONVERTED TO NUMBER('50685')
-// ASSIGN THAT NUMBER AS PRIMARYVALUE
-// STORE OPERATOR CHOSEN 
-// REPEAT ABOVE TO ASSIGN NUMBER TO SECONDARYVALUE
-// ADD EVENT LISTENER TO '=' THAT CALLS THE RUNOPERATE() FUNC & PASSES ALL ARGS WHEN CLICKED
-// ASSIGN RUNOPERATE OUTPUT TO AN ANSWER VARIABLE AND DISPLAY WHILE REMOVING OTHER VALUES
-
+//------------------------------------------------------------------------------------
 
 
 
 
-
-
-
-// FUNCTIONS THAT POPULATE THE DISPLAY
-// ADD LISTENERS TO ALL BUTTONS
-let buttons = document.querySelectorAll('.buttons') 
-buttons.forEach((button) => {
-    button.addEventListener('click', function (event) {
-       let buttonValue = [] 
-       buttonValue.push(Number(event.target.innerHTML))
-       
-       
-       
-       displayFunction(buttonValue)
-       console.log(buttonValue)
-
-    })
-
-})
 
 // PASS NUMBERS EVENT FIRED ON TO DISPLAYING FUNCTION
 let screenDisplay = document.getElementById("screen")
-function displayFunction(buttonValue) {
-    screenDisplay.innerText = buttonValue
+function displayFunction(primaryNumber, primaryOperator, secondaryNumber ) {
+
+
+
+    screenDisplay.innerText = primaryNumber + primaryOperator + secondaryNumber
     
 }
+
