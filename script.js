38e39556fea8e59b2c1a15c1ec87cc660e397bd5
Simran Johal <134616481+simran-johal@users.noreply.github.com>
2023-08-28 17:39:41 +0100

    Complete update to calculator logic
    - Add chained operations functionality

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 
 
-let screenNumPrimary = document.getElementById("screen-num-primary")
-let screenNumSecondary = document.getElementById("screen-num-secondary")
-let buttons = document.querySelectorAll('.buttons')
+const screenNumPrimary = document.getElementById("screen-num-primary")
+const screenNumSecondary = document.getElementById("screen-num-secondary")
+const buttons = document.querySelectorAll('.buttons')
+const operatorObj = { 
+    add: (num1,num2) => num1 + num2,
+    subtract: (num1,num2) => num1 - num2,
+    divide: (num1,num2) => num1 / num2,
+    multiply: (num1,num2) => num1 * num2,
+}
 
 let primaryNumber = null; 
 let secondaryNumber = null;
@@ -10,49 +16,46 @@ let displayOperator = null;
 let calcOperator = null
 let currentNumber = "";
 let result = null;
+let chainedOperation = false
 
-let operatorObj = { 
-    add: (num1,num2) => num1 + num2,
-    subtract: (num1,num2) => num1 - num2,
-    divide: (num1,num2) => num1 / num2,
-    multiply: (num1,num2) => num1 * num2,
-}
-
-
+const numbersRegex = /^[0-9]$/
+const operatorsRegex = /[\+\-x÷]/
 
 function displayFunction({ currentNumber = "", primaryNumber = "", displayOperator = "", secondaryNumber = "", result = "" }) {
 
     let displayTextPrimary = ""
     let displayTextSecondary = ""
   
-    if (result !== null) {                                                          
+    if (chainedOperation) {
+        displayTextPrimary = `${primaryNumber} ${displayOperator} ${secondaryNumber}`
+        displayTextSecondary = ""
+            consoleLogger()
+
+    } else if (result !== null) {                                                          
         displayTextPrimary = `${primaryNumber} ${displayOperator} ${secondaryNumber}`; // state equals clicked, no currentNumber but have the rest
         displayTextSecondary = `= ${result}`    
-    } 
-   
-    else if (currentNumber !== "" && primaryNumber !== null && displayOperator !== null) { // state new currentNum but primaryNum and currentOperator too
-        displayTextPrimary = `${primaryNumber} ${displayOperator} ${currentNumber}`;
-    }
-    
-    else if (primaryNumber !== null && displayOperator !== null) { // state have a primaryNumber and cprimaryOperator 
-        displayTextPrimary = `${primaryNumber} ${displayOperator}`;
-    }
-    
-    else displayTextPrimary = currentNumber // state displaying the current number as we click updating it
 
-    
+    } else if (currentNumber !== "" && primaryNumber !== null && displayOperator !== null) { // state new currentNum but primaryNum and currentOperator too
+        displayTextPrimary = `${primaryNumber} ${displayOperator} ${currentNumber}`;
+
+    } else if (primaryNumber !== null && displayOperator !== null) { // state have a primaryNumber and cprimaryOperator 
+        displayTextPrimary = `${primaryNumber} ${displayOperator}`;
+
+    } else displayTextPrimary = currentNumber // state displaying the current number as we click updating it
+
 
     screenNumPrimary.innerText = displayTextPrimary
     screenNumSecondary.innerText = displayTextSecondary
 
 }
 
-function resetCalculator(newPrimary = null) {
+function resetCalculator(newPrimary = null, newDisplayOperator = null) {
     primaryNumber = newPrimary
     secondaryNumber = null;
-    displayOperator = null;
+    displayOperator = newDisplayOperator
     currentNumber = ""
     result = null; 
+    chainedOperation = false
    
 }
 
@@ -61,74 +64,92 @@ buttons.forEach((button) => {
 
        const content = event.target.innerHTML // capture the inner HTML
 
-       if (/^[0-9]$/.test(content)) { // HANDLING NUMBERS //
-
-            if (result !== null) { // if already have result
-                resetCalculator()
-            } currentNumber += content // if dont have result appending with new number 
-            displayFunction({ currentNumber, primaryNumber, displayOperator, secondaryNumber, result })
-        }
-       
-       else if (/[\+\-x÷]/.test(content)) { // HANDLING OPERATORS //
-            
-            if (result !== null) { // already have a result assign it to primaryNumber
-                primaryNumber = result
-                resetCalculator(result)
-            }
-            if (primaryNumber === null) { // no primary number means no result
-                primaryNumber = currentNumber // so set your current number to primary
-            } else {
-                secondaryNumber = currentNumber // if result isnt set to primary + already have a primary
-            }
-            
-
-            switch (content) {
-                case '+':
-                    calcOperator = 'add';
-                    break;
-                case '-':
-                    calcOperator = 'subtract';
-                    break;
-                case 'x':
-                    calcOperator = 'multiply';
-                    break;
-                case '÷':
-                    calcOperator = 'divide';
-                    break;
-            }
-
-            displayOperator = content
-            currentNumber = ""
-            displayFunction({ currentNumber, primaryNumber, displayOperator, secondaryNumber, result })
-            
-       }
-
-       else if (/=/.test(event.target.innerHTML)) { // HANDLING EQUALS //
-
-            if (primaryNumber !== null && calcOperator !== null) { // we have a primary num and operator
-                
-                console.log("primary num", primaryNumber)
-                console.log("operator", calcOperator)
-                console.log("secondary num", secondaryNumber = currentNumber) // assign current num to secondary number
-                runOperate(calcOperator, parseFloat(primaryNumber), parseFloat(secondaryNumber)) // pass it all to runOperate + convert strings to numbers
-                
-            }
-    
-            currentNumber = ""
-            displayFunction({ currentNumber, primaryNumber, displayOperator, secondaryNumber, result })
-       }
-
+       if (numbersRegex.test(content)) handleNumbers(content) 
+       else if (operatorsRegex.test(content)) handleOperators(content)
+       else if (/=/.test(event.target.innerHTML)) handleEquals()
 
     })
 
 })
 
+function handleNumbers(content) {
 
-function runOperate(calcOper, primNum, seconNUM) {
-    result = operatorObj[calcOper](primNum, seconNUM)
+    if (result !== null && !chainedOperation) { // if already have result
+        resetCalculator()
+    } else if (chainedOperation) {
+        resetCalculator(primaryNumber, displayOperator)
+    }  
+    
+    currentNumber += content // if dont have result appending with new number 
+    displayFunction({ currentNumber, primaryNumber, displayOperator, secondaryNumber, result })
+
+        consoleLogger()
+}
+
+function handleOperators(content) {
+    if (result !== null) { // already have a result assign it to primaryNumber
+        primaryNumber = result
+        resetCalculator(result)
+    }
+
+    if (primaryNumber !== null && currentNumber !== "") { // you have a primary number and a new number
+
+        secondaryNumber = currentNumber; // where no result but have primary, current number = secondary number
+        runOperate(calcOperator, parseFloat(primaryNumber), parseFloat(secondaryNumber)); // perform the operation
+        primaryNumber = result; // now result becomes the new primary number
+        chainedOperation = true
+        secondaryNumber = ""
+        
+    }
+
+    else if (primaryNumber === null) { // no primary number 
+        primaryNumber = currentNumber // so set your current number to primary 
+    }
+
+    switch (content) {
+        case '+': calcOperator = 'add'; break;
+        case '-': calcOperator = 'subtract'; break;
+        case 'x': calcOperator = 'multiply'; break;
+        case '÷': calcOperator = 'divide'; break;
+    }
+
+    displayOperator = content
+    currentNumber = ""
+    displayFunction({ currentNumber, primaryNumber, displayOperator, secondaryNumber, result })
+
+        consoleLogger()
+}
+
+function handleEquals() {
+    if (primaryNumber !== null && calcOperator !== null) { // we have a primary num and operator
+        secondaryNumber = currentNumber // assign current num to secondary number
+        runOperate(calcOperator, parseFloat(primaryNumber), parseFloat(secondaryNumber)) // pass it all to runOperate + convert strings to numbers
+        
+    }
+
+    currentNumber = ""
+    displayFunction({ currentNumber, primaryNumber, displayOperator, secondaryNumber, result })
+    chainedOperation = false
+
+        consoleLogger()
+}
+
+function runOperate(calcOper, primaryNum, secondaryNUM) {
+    result = operatorObj[calcOper](primaryNum, secondaryNUM)
     displayFunction({ currentNumber, primaryNumber, displayOperator, secondaryNumber, result })
     
     
+}
+
+function consoleLogger() {
+    console.log("-------")
+        console.log("primary number", primaryNumber)
+        console.log("secondary number", secondaryNumber)
+        console.log("current number", currentNumber)
+        console.log("calc operator", calcOperator)
+        console.log("display operator", displayOperator)
+        console.log("result", result)
+
 }
 
 
@@ -152,8 +173,9 @@ function runOperate(calcOper, primNum, seconNUM) {
 
 
 
-
-
-// COMPLETE CALCULATOR TO WORK 
 // ADD DELETE AND CLEAR LOGIC
+// FIRST NUMBER CANT BE ZERO
 // COMPLETE UI
+// BETTER WAY TO WRITE MY LOGIC NOT ALL IN THE ONE EVENT HANDLER
+
+
